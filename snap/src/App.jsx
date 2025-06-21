368ee8542083beb82e0f307d65618c69c228bd81
midestic <badmususman50@gmail.com>
2025-06-21 14:49:32 +0100

    fix

--- snap/src/App.jsx
+++ snap/src/App.jsx
@@ -5,8 +5,10 @@ import Navbar from "./components/navbar/Navbar";
 function App() {
   return (
     <div className="app">
-      <Navbar />
-      <Hero />
+      <div className="box">
+        <Navbar />
+        <Hero />
+      </div>
     </div>
   );
 }
